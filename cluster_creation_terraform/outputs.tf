11239b81260e0894df1ecfcd76828a00b7ec4ac8
grantneumanoracle <grant.neuman@oracle.com>
2025-03-19 16:11:56 -0700

    add cluster terraform

---
+++ cluster_creation_terraform/outputs.tf
@@ -0,0 +1,10 @@
+# Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+# 
+
+output "deploy_id" {
+  value = random_string.deploy_id.result
+}
+output "deployed_to_region" {
+  value = var.region
+}
