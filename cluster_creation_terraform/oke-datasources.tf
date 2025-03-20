4126f3888e959510434d650492f1fd2a77ff6f16
grantneumanoracle <grant.neuman@oracle.com>
2025-03-20 11:59:02 -0700

    remove constraint to use kubernetes version in image name

--- cluster_creation_terraform/oke-datasources.tf
+++ cluster_creation_terraform/oke-datasources.tf
@@ -8,12 +8,12 @@
 # }
 
 # Gets a list of supported images based on the shape, operating_system and operating_system_version provided
-data "oci_core_images" "node_pool_images" {
-  compartment_id   = local.oke_compartment_ocid
-  operating_system = var.image_operating_system
-  shape            = var.node_pool_instance_shape.instanceShape
-  sort_by          = "TIMECREATED"
-  sort_order       = "DESC"
+data "oci_core_images" "shape_specific_images" {
+  compartment_id = local.oke_compartment_ocid
+  #operating_system = var.image_operating_system
+  shape = var.node_pool_instance_shape.instanceShape
+  #sort_by          = "TIMECREATED"
+  #sort_order       = "DESC"
 }
 
 data "oci_containerengine_node_pool_option" "cluster_node_pool_option" {
@@ -88,40 +88,19 @@ resource "random_string" "app_name_autogen" {
 }
 
 locals {
+
   # Extract k8s version without the 'v' prefix
   cluster_k8s_version_no_v = replace(oci_containerengine_cluster.oke_cluster[0].kubernetes_version, "v", "")
 
-  # Map of available OKE-specific images from node_pool_option
-  available_images = {
-    for src in data.oci_containerengine_node_pool_option.cluster_node_pool_option.sources :
-    src.image_id => src.source_name
-  }
+  all_shape_compatible_images   = data.oci_core_images.shape_specific_images.images
+  all_cluster_compatible_images = data.oci_containerengine_node_pool_option.cluster_node_pool_option.sources
 
-  # Find images that match the k8s version (strict matching)
-  k8s_version_strict_match_images = {
-    for id, name in local.available_images :
-    id => name
-    if strcontains(name, "-OKE-${local.cluster_k8s_version_no_v}-")
-  }
+  all_shape_compatible_image_ids = [for image in local.all_shape_compatible_images : image.id]
 
-  # Find images that just contain the k8s version number
-  k8s_version_loose_match_images = {
-    for id, name in local.available_images :
-    id => name
-    if strcontains(name, local.cluster_k8s_version_no_v)
-  }
+  all_cluster_compatible_image_ids = [for source in local.all_cluster_compatible_images : source.image_id]
 
-  # Get the first compatible image id
-  first_compatible_image_id = try(
-    keys(local.k8s_version_strict_match_images)[0],
-    try(
-      keys(local.k8s_version_loose_match_images)[0],
-      try(
-        keys(local.available_images)[0],
-        null
-      )
-    )
-  )
+  first_compatible_image_id = tolist(setintersection(toset(local.all_shape_compatible_image_ids), toset(local.all_cluster_compatible_image_ids)))[0]
+
 }
 
 # Debug outputs
@@ -129,40 +108,14 @@ output "debug_cluster_k8s_version_no_v" {
   value = local.cluster_k8s_version_no_v
 }
 
-output "debug_available_images" {
-  value = local.available_images
+output "debug_all_shape_compatible_images" {
+  value = local.all_shape_compatible_images
+}
+
+output "debug_all_cluster_compatible_images" {
+  value = local.all_cluster_compatible_images
 }
 
 output "debug_first_compatible_image_id" {
   value = local.first_compatible_image_id
-}
-
-output "debug_k8s_version_strict_match_images" {
-  value       = local.k8s_version_strict_match_images
-  description = "Images that exactly match the k8s version pattern"
-}
-
-output "debug_k8s_version_loose_match_images" {
-  value       = local.k8s_version_loose_match_images
-  description = "Images that contain the k8s version number"
-}
-
-output "debug_image_selection_explanation" {
-  value = try(
-    length(local.k8s_version_strict_match_images) > 0
-    ? "Found image matching k8s version (strict matching)"
-    : length(local.k8s_version_loose_match_images) > 0
-    ? "Found image matching k8s version (loose matching)"
-    : length(local.available_images) > 0
-    ? "Using fallback: any available image"
-    : "No images available"
-  , "No images available")
-}
-
-output "debug_node_pool_images" {
-  value = data.oci_core_images.node_pool_images
-}
-
-output "debug_node_pool_option" {
-  value = data.oci_containerengine_node_pool_option.cluster_node_pool_option
 }
